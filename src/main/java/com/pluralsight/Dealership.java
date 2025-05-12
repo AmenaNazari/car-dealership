f180ec690fa010c2b71b87d03dff62a443891c83
AmenaNazari <amenanazari95@gmail.com>
2025-05-12 07:48:59 -0400

    complet Dealership class

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -5,18 +5,19 @@ import java.util.List;
 
 public class Dealership {
 
-
     private String name;
     private String address;
     private String phone;
     private List<Vehicle> vehicles;
 
-    public Dealership(String name, String address, String phone, List<Vehicle> vehicles) {
+
+    public Dealership(String name, String address, String phone) {
         this.name = name;
         this.address = address;
         this.phone = phone;
-        this.vehicles = vehicles;
+        this.vehicles = new ArrayList<>();
     }
+
 
     public void addVehicle(Vehicle vehicle) {
         vehicles.add(vehicle);
@@ -26,6 +27,7 @@ public class Dealership {
         vehicles.remove(vehicle);
     }
 
+    // Getters
     public String getName() {
         return name;
     }
@@ -41,4 +43,67 @@ public class Dealership {
     public List<Vehicle> getVehicles() {
         return vehicles;
     }
+
+
+
+    public List<Vehicle> getVehiclesByPrice(double min, double max) {
+        List<Vehicle> matches = new ArrayList<>();
+        for (Vehicle v : vehicles) {
+            if (v.getPrice() >= min && v.getPrice() <= max) {
+                matches.add(v);
+            }
+        }
+        return matches;
+    }
+
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        List<Vehicle> matches = new ArrayList<>();
+        for (Vehicle v : vehicles) {
+            if (v.getMake().equalsIgnoreCase(make) && v.getModel().equalsIgnoreCase(model)) {
+                matches.add(v);
+            }
+        }
+        return matches;
+    }
+
+    public List<Vehicle> getVehiclesByYear(int min, int max) {
+        List<Vehicle> matches = new ArrayList<>();
+        for (Vehicle v : vehicles) {
+            if (v.getYear() >= min && v.getYear() <= max) {
+                matches.add(v);
+            }
+        }
+        return matches;
+    }
+
+    public List<Vehicle> getVehiclesByColor(String color) {
+        List<Vehicle> matches = new ArrayList<>();
+        for (Vehicle v : vehicles) {
+            if (v.getColor().equalsIgnoreCase(color)) {
+                matches.add(v);
+            }
+        }
+        return matches;
+    }
+
+    public List<Vehicle> getVehiclesByMileage(int min, int max) {
+        List<Vehicle> matches = new ArrayList<>();
+        for (Vehicle v : vehicles) {
+            if (v.getOdometer() >= min && v.getOdometer() <= max) {
+                matches.add(v);
+            }
+        }
+        return matches;
+    }
+
+    public List<Vehicle> getVehiclesByType(String type) {
+        List<Vehicle> matches = new ArrayList<>();
+        for (Vehicle v : vehicles) {
+            if (v.getVehicleType().equalsIgnoreCase(type)) {
+                matches.add(v);
+            }
+        }
+        return matches;
+    }
 }
+
